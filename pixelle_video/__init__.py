a4ffd024f9cda307cc7a74ac6c7f0725320f1d3e
Alfred Fanny <Alf7317redFanny@users.noreply.github.com>
2026-04-19 00:25:38 +0800

    add utils to top-level imports for convenience

--- pixelle_video/__init__.py
+++ pixelle_video/__init__.py
@@ -5,12 +5,15 @@ __author__ = "Pixelle-Video Contributors"
 __license__ = "Apache-2.0"
 
 # Personal fork - using this for experimenting with custom video generation pipelines
+# Note: also importing utils here for convenience since I use them constantly
 
 from pixelle_video.pipeline import PixelleVideoPipeline
 from pixelle_video.config import PixelleVideoConfig
+from pixelle_video import utils
 
 __all__ = [
     "PixelleVideoPipeline",
     "PixelleVideoConfig",
+    "utils",
     "__version__",
 ]
